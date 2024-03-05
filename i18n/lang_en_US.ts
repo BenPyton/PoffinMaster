076ae991583d1b9f829b3f665bba34ac18534954
BenPyton <ben.pyton@gmail.com>
2024-03-05 16:24:02 +0100

    Updated translation files

--- i18n/lang_en_US.ts
+++ i18n/lang_en_US.ts
@@ -4,18 +4,18 @@
 <context>
     <name>App</name>
     <message>
-        <location filename="../content/App.qml" line="46"/>
+        <location filename="../content/App.qml" line="60"/>
         <source>?</source>
         <extracomment>Opens the &apos;about&apos; popup</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../content/App.qml" line="114"/>
+        <location filename="../content/App.qml" line="128"/>
         <source>about</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../content/App.qml" line="130"/>
+        <location filename="../content/App.qml" line="144"/>
         <source>&lt;b&gt;Poffin Master&lt;/b&gt; v%1
                 &lt;br/&gt;Developed by %3
                 &lt;br/&gt;Source code is under license &lt;a href=&quot;%5&quot; style=&quot;color: %7;&quot;&gt;%4&lt;/a&gt;
@@ -24,7 +24,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../content/App.qml" line="155"/>
+        <location filename="../content/App.qml" line="169"/>
         <source>Berries and poffins images are used under the &quot;fair use&quot; and are owned by Game Freaks and ILCA.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -32,42 +32,42 @@
 <context>
     <name>Poffin</name>
     <message>
-        <location filename="../Backend/poffin.cpp" line="136"/>
+        <location filename="../Backend/poffin.cpp" line="153"/>
         <source>poffin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="147"/>
+        <location filename="../Backend/poffin.cpp" line="164"/>
         <source>foul %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="158"/>
+        <location filename="../Backend/poffin.cpp" line="175"/>
         <source>%2 %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="165"/>
+        <location filename="../Backend/poffin.cpp" line="182"/>
         <source>%2-%3 %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="170"/>
+        <location filename="../Backend/poffin.cpp" line="187"/>
         <source>overripe %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="176"/>
+        <location filename="../Backend/poffin.cpp" line="193"/>
         <source>rich %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="180"/>
+        <location filename="../Backend/poffin.cpp" line="197"/>
         <source>mild %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="184"/>
+        <location filename="../Backend/poffin.cpp" line="201"/>
         <source>super mild %1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -75,52 +75,52 @@
 <context>
     <name>PoffinStatsModel</name>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="6"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="23"/>
         <source>spicy</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="6"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="23"/>
         <source>coolness</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="7"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="24"/>
         <source>dry</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="7"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="24"/>
         <source>beauty</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="8"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="25"/>
         <source>sweet</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="8"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="25"/>
         <source>cuteness</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="9"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="26"/>
         <source>bitter</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="9"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="26"/>
         <source>cleverness</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="10"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="27"/>
         <source>sour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="10"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="27"/>
         <source>toughness</source>
         <translation type="unfinished"></translation>
     </message>
@@ -128,19 +128,19 @@
 <context>
     <name>Screen01</name>
     <message>
-        <location filename="../content/Screen01.qml" line="50"/>
+        <location filename="../content/Screen01.qml" line="59"/>
         <source>%1 LV.%2 (x%3)</source>
         <extracomment>The resulting poffin string. %1 is the poffin name (e.g. Rich, Sweet-Bitter, Foul) %2 is the poffin level, %3 is the number of poffin produced</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../content/Screen01.qml" line="62"/>
+        <location filename="../content/Screen01.qml" line="71"/>
         <source>smoothness</source>
         <extracomment>The smoothness of the berry.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../content/Screen01.qml" line="155"/>
+        <location filename="../content/Screen01.qml" line="164"/>
         <source>Select a berry</source>
         <extracomment>Text displayed when no berry is selected</extracomment>
         <translation type="unfinished"></translation>
--- i18n/lang_fr_FR.ts
+++ i18n/lang_fr_FR.ts
@@ -4,18 +4,18 @@
 <context>
     <name>App</name>
     <message>
-        <location filename="../content/App.qml" line="46"/>
+        <location filename="../content/App.qml" line="60"/>
         <source>?</source>
         <extracomment>Opens the &apos;about&apos; popup</extracomment>
         <translation>?</translation>
     </message>
     <message>
-        <location filename="../content/App.qml" line="114"/>
+        <location filename="../content/App.qml" line="128"/>
         <source>about</source>
         <translation>à propos</translation>
     </message>
     <message>
-        <location filename="../content/App.qml" line="130"/>
+        <location filename="../content/App.qml" line="144"/>
         <source>&lt;b&gt;Poffin Master&lt;/b&gt; v%1
                 &lt;br/&gt;Developed by %3
                 &lt;br/&gt;Source code is under license &lt;a href=&quot;%5&quot; style=&quot;color: %7;&quot;&gt;%4&lt;/a&gt;
@@ -56,7 +56,7 @@
                 &lt;br/&gt;Le code source est disponible sur &lt;a href=&quot;%6&quot; style=&quot;color: %7;&quot;&gt;Github&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../content/App.qml" line="155"/>
+        <location filename="../content/App.qml" line="169"/>
         <source>Berries and poffins images are used under the &quot;fair use&quot; and are owned by Game Freaks and ILCA.</source>
         <translation>Les images des baies et des poffins sont la propriété de Game Feaks et ILCA.</translation>
     </message>
@@ -88,42 +88,42 @@
         <translation type="vanished">Super Doux</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="136"/>
+        <location filename="../Backend/poffin.cpp" line="153"/>
         <source>poffin</source>
         <translation>poffin</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="147"/>
+        <location filename="../Backend/poffin.cpp" line="164"/>
         <source>foul %1</source>
         <translation>%1 infect</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="158"/>
+        <location filename="../Backend/poffin.cpp" line="175"/>
         <source>%2 %1</source>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="165"/>
+        <location filename="../Backend/poffin.cpp" line="182"/>
         <source>%2-%3 %1</source>
         <translation>%1 %2 %3</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="170"/>
+        <location filename="../Backend/poffin.cpp" line="187"/>
         <source>overripe %1</source>
         <translation>%1 écœurant</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="176"/>
+        <location filename="../Backend/poffin.cpp" line="193"/>
         <source>rich %1</source>
         <translation>%1 nourrissant</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="180"/>
+        <location filename="../Backend/poffin.cpp" line="197"/>
         <source>mild %1</source>
         <translation>%1 doux</translation>
     </message>
     <message>
-        <location filename="../Backend/poffin.cpp" line="184"/>
+        <location filename="../Backend/poffin.cpp" line="201"/>
         <source>super mild %1</source>
         <translation>%1 super doux</translation>
     </message>
@@ -171,52 +171,52 @@
         <translation type="vanished">Robustesse</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="6"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="23"/>
         <source>spicy</source>
         <translation>épicé</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="6"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="23"/>
         <source>coolness</source>
         <translation>sang-froid</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="7"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="24"/>
         <source>dry</source>
         <translation>âpre</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="7"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="24"/>
         <source>beauty</source>
         <translation>beauté</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="8"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="25"/>
         <source>sweet</source>
         <translation>sucré</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="8"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="25"/>
         <source>cuteness</source>
         <translation>grâce</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="9"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="26"/>
         <source>bitter</source>
         <translation>amer</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="9"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="26"/>
         <source>cleverness</source>
         <translation>intelligence</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="10"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="27"/>
         <source>sour</source>
         <translation>acide</translation>
     </message>
     <message>
-        <location filename="../Backend/poffinstatsmodel.cpp" line="10"/>
+        <location filename="../Backend/poffinstatsmodel.cpp" line="27"/>
         <source>toughness</source>
         <translation>robustesse</translation>
     </message>
@@ -239,19 +239,19 @@
         <translation type="vanished">Onctuosité   %1</translation>
     </message>
     <message>
-        <location filename="../content/Screen01.qml" line="50"/>
+        <location filename="../content/Screen01.qml" line="59"/>
         <source>%1 LV.%2 (x%3)</source>
         <extracomment>The resulting poffin string. %1 is the poffin name (e.g. Rich, Sweet-Bitter, Foul) %2 is the poffin level, %3 is the number of poffin produced</extracomment>
         <translation>%1 N. %2 (x%3)</translation>
     </message>
     <message>
-        <location filename="../content/Screen01.qml" line="62"/>
+        <location filename="../content/Screen01.qml" line="71"/>
         <source>smoothness</source>
         <extracomment>The smoothness of the berry.</extracomment>
         <translation>onctuosité</translation>
     </message>
     <message>
-        <location filename="../content/Screen01.qml" line="155"/>
+        <location filename="../content/Screen01.qml" line="164"/>
         <source>Select a berry</source>
         <extracomment>Text displayed when no berry is selected</extracomment>
         <translation>Choisissez une baie</translation>
